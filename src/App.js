7450affbe5484da535442c3905f91f6f29916697
Tatyana Shevchenko <t.shevchenko@mafin.ru>
2021-05-16 19:46:06 +0300

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Pallete } from "./components/pallete";
 function App() {
 
   const [colorPallette, setColorPallete] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   function getRandomColor() {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -25,10 +26,39 @@ function App() {
     setColorPallete(colors);
   }
 
+  // function onInputChange(e){
+  //   setInputValue(e.target.value);
+  // }
+
+  // function runEval(){
+  //   eval(inputValue);
+  // }
+
+print(1);
+setTimeout(()=>print(5),0)
+print(2);
+print(3);
+const promise1 = new Promise((resolve, reject) => {
+  resolve(4);
+});
+const promise2= new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(6);
+  }, 300);
+});
+promise1.then( res => print(res));
+promise2.then( res => print(res));
+
+function print (n) {
+  console.log(n);
+}
+
   return (
     <div className="App">
       <div className="App__container">
       <h1 className="Title">Wanna get an awefully awesome color palette?</h1>
+      {/* <input type="text" onChange={e=>onInputChange(e)} value={inputValue}></input>
+      <button onClick={runEval}>EVAL</button> */}
       <p className="Description">
         Use my extremely unintelligent random color palette creator
       </p>
